bcfa4e3d817e1370beee2d39eb878685b36500ba
jjblues86 <jeromejoof@yahoo.com>
2019-03-13 21:23:18 -0700

    progress

--- src/auth/users-model.js
+++ src/auth/users-model.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const users = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
@@ -27,7 +29,7 @@ users.statics.authenticateBasic = function(auth) {
 
 // Compare a plain text password against the hashed one we have saved
 users.methods.comparePassword = function(password) {
-    return bcrypt.compare(password, this.password);
+    return bcrypt.compare(password, this.password, (err, data) => data);
 };
 
 // Generate a JWT from the user id and a secret
